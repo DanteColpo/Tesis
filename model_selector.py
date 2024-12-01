ae5f9b887587d3e7fd73e043dcfa7a10d580f906
DanteColpo <dante.colpo@gmail.com>
2024-12-01 16:34:26 -0300

    Update model_selector.py

--- model_selector.py
+++ model_selector.py
@@ -92,7 +92,8 @@ def generate_graph(data, selected_models, all_results):
         x=data_monthly.index,
         y=data_monthly['CANTIDAD'],
         mode='lines',
-        name='Datos Históricos'
+        name='Datos Históricos',
+        line=dict(color='blue')
     ))
 
     # Agregar las proyecciones de los modelos seleccionados
